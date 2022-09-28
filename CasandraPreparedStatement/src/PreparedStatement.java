35c40f81a70e3f7c342095a7744295b4bab3bde7
usb777 <dzsamoila@gmail.com>
2022-09-27 23:11:54 -0400

    Readme update

---
+++ CasandraPreparedStatement/src/PreparedStatement.java
@@ -0,0 +1,13 @@
+
+public class PreparedStatement {
+	private final static String INSERT_QUERY = "INSERT INTO emp (emp_id, emp_name, emp_city, emp_phone, emp_sal) VALUES(?,?,?,?,?)";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("Prepared statement");
+  
+  
+  
+	}
+
+}
